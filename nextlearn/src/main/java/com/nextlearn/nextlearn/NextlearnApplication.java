73c2718414e0f9ae128f6c33b0e0208de9037f24
shilma-t <shilmathanzeel5@gmail.com>
2025-04-16 08:18:32 +0530

    Initial commit with backend and frontend

---
+++ nextlearn/src/main/java/com/nextlearn/nextlearn/NextlearnApplication.java
@@ -0,0 +1,13 @@
+package com.nextlearn.nextlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextlearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextlearnApplication.class, args);
+	}
+
+}
