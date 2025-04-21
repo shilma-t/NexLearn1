aadf10030dc4de947c4bdf94c59b9024228ada6b
Amandhi Siriwardena <amandhisiriwardena66@gmail.com>
2025-04-21 23:44:34 +0530

    Progress new

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import CreateProgress from './pages/Progress/ProgressCreation';
 import AllProgress from './pages/Progress/AllProgress'; 
-import UserProgress from './pages/Progress/UserProgress';
 
 const App = () => {
   return (
@@ -12,7 +11,7 @@ const App = () => {
         <Route path="/" element={<></>} />
         <Route path="/create-progress" element={<CreateProgress />} />
         <Route path="/all-progress" element={<AllProgress />} /> 
-        <Route path="/user-progress" element={<UserProgress/>} /> 
+      
       </Routes>
     </Router>
   );
