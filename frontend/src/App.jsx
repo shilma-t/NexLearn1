1c5c30c9d64686701a7a0a214788b5c5829a420e
Amandhi Siriwardena <amandhisiriwardena66@gmail.com>
2025-04-22 18:23:34 +0530

    edit and delete frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,9 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import CreateProgress from './pages/Progress/ProgressCreation';
 import AllProgress from './pages/Progress/AllProgress'; 
+import UserProgress from './pages/Progress/UserProgress';
+import ProgressEdit from './pages/Progress/ProgressEdit';
+
 
 const App = () => {
   return (
@@ -11,7 +14,10 @@ const App = () => {
         <Route path="/" element={<></>} />
         <Route path="/create-progress" element={<CreateProgress />} />
         <Route path="/all-progress" element={<AllProgress />} /> 
+        <Route path="/my-progress" element={<UserProgress />} />
+        <Route path="/edit-progress/:id" element={<ProgressEdit />} />
       
+        
       </Routes>
     </Router>
   );
